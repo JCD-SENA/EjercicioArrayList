195f53ab18ea2322d60895f12d0fd9cb0903fe57
JCD-SENA <161314582+JCD-SENA@users.noreply.github.com>
2024-06-05 15:14:57 -0500

    Update Principal.java

--- completo/juanca/principal/Principal.java
+++ completo/juanca/principal/Principal.java
@@ -2,6 +2,7 @@ package juanca.principal;
 
 public class Principal {
     public static void main(String[] args) {
+        //Se crea una nueva clase ProfesoresEstudiantes y se ejecuta la función inicar
         ProfesoresEstudiantes procesos = new ProfesoresEstudiantes();
         procesos.iniciar();
     }
